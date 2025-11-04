a66316e1f782a6f27e3450e38192af62f91f8bcb
ALEKSANDR.I <107239203+AlexRMB1455AL@users.noreply.github.com>
2025-11-05 01:53:13 +0400

    ttt_+win_err2391

--- ttt_main.py
+++ ttt_main.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+
 """
 1. Нам нужно отобразить доску (игровое поле).
 2. Получить ввод от игрока.
@@ -8,15 +8,15 @@
 6. Спросить, хотят ли игроки играть снова."""
 
 #Нам нужно отобразить доску (игровое поле)
-#-------------------------------------------------------------------------------
-#map_go*************************************************************************
+
+#map_go
 result = {1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'} #const
 result_play = {1:' ',2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' '} #const
 result_players = {1:' ',2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' '} #change_inplay
-win_results = [(1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)]
-#map_end************************************************************************
-#-------------------------------------------------------------------------------
-#description_go*****************************************************
+win_results = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+#map_end
+
+#description_go
 start_des = 'Описание ячеек ввода такое же как на клавиатуре справа'
 play_des =  'Игровая площадка с номерами ячеек в диапазоне 1-9'
 play_des1 = 'Игрок 1 использует "Х", а игрок 2 использует "0"'
@@ -34,12 +34,13 @@ play_des11 = 'Символ не верный, повторите ввод'
 ind10 =' '*10
 ind4 = ' '*4
 ind0 = ''
-#description_end******************************************************
-#-------------------------------------------------------------------------------
+#description_end
+
 
 
 #Function
-#start_board_go*****************************************************************
+#start_board_go
+
 def board_place(run, indent='',description1='', description2='',description3=''):
     A0=A6='-'*7
     A1='|'+run[7]+'|'+run[8]+ '|' +run[9]+ '|'+ indent + '* ' + description1
@@ -48,37 +49,49 @@ def board_place(run, indent='',description1='', description2='',description3='')
     A2=A4='|'+'-'*5 +'|'
     print(ind0)
     for place_num in [A0,A1,A2,A3,A4,A5,A6]:
-        print(ind10,place_num)
-#start_board_end****************************************************************
-#-------------------------------------------------------------------------------
-#clear_terminal_go**********************************
+        print(ind10, place_num)
+
+#start_board_end
+
+#clear_terminal_go
 import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
-#clear_terminal_end*********************************
-#input_go***********************************************************
+
+#clear_terminal_end
+
+#input_go
+
 def check_input(marker):
     number = input(f'{ind10}{play_des8},{play_des10},{marker}:')
     return number
-#input_end**********************************************************
-#update_board_go*********************************************
+
+#input_end
+
+#update_board_go
+
 def board_up(result_board):
     clear()
-    board_place(result_board,ind4,play_des,play_des1,play_des2)
-#update_board_end*********************************************
+    board_place(result_board, ind4, play_des, play_des1, play_des2)
+
+#update_board_end
+
 #Function_end
-#-------------------------------------------------------------------------------
-#start_play_go*************************
+
+#start_play_go
+
 board_place(result,ind4,ind0,start_des)
-board_place(result_play,ind4,ind0,play_des)
-#start_play_end************************
-#-------------------------------------------------------------------------------
-#start_game_go***************************************************
+board_place(result_play, ind4, ind0, play_des)
+
+#start_play_end
+
+#start_game_go
+
 flag = 1
 
 while flag == 1:
     try:
-        start_checkin = input(f'{ind10},{play_des5}')
+        start_checkin = input(f'{ind10} {play_des5}')
         print(ind0)
         if start_checkin == 'Y' or start_checkin == 'y':
             board_up(result_play)
@@ -96,36 +109,36 @@ while flag == 1:
 
 def game_check(tic_toc):
     for win_check in win_results:
-        if (result_players[win_check[0]] == tic_toc and
-            result_players[win_check[1]] == tic_toc and
+        if (result_players[win_check[0]] ==
+            result_players[win_check[1]] ==
             result_players[win_check[2]] == tic_toc):
             print(f'{ind10} {tic_toc} - Победил!')
             return 1
-        else: 
+        else:
             return 0
-        
+
 #if ' ' not in result_players.values():
 #             print(f'{ind10} Ничья!')
 #             flag = 1
 #             break
-#start_game_end*****************************************************
-#-------------------------------------------------------------------------------
-#players_move_go******************************************
+#start_game_end
+
+#players_move_go
 def players_move_X_0(literal_tic_toc, flag):
     while flag == 0:
         try:
             play_str = check_input(literal_tic_toc)
         except KeyboardInterrupt:
             print(f"{ind10}{play_des6}")
-            break
+            return 1
         if play_str in result.values():
             play2 = int(play_str)
-            flag = 1
-            if  result_players[play2] == ' ':
+            if result_players[play2] == ' ':
                 result_players[play2] = literal_tic_toc
+                flag = 1
                 board_up(result_players)
-                if game_check(literal_tic_toc) == 1:
-                    
+                checking_out = game_check(literal_tic_toc)
+                if checking_out == 1:
                     return 1
             else:
                 print(ind10,play_des4)
@@ -133,7 +146,7 @@ def players_move_X_0(literal_tic_toc, flag):
             print(ind10,play_des3)
 
 
-#players_move_end*****************************************
+#players_move_end
 
 while True:
         if flag == 1:
@@ -141,4 +154,8 @@ while True:
             break
         else:
             flag = players_move_X_0('X', 0)
+            if flag == 1:
+                print(f'{ind10} Игра окончена')
+                break
+
             flag = players_move_X_0('0', 0)
